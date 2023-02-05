4a80fb01ed825b136f5f7db3a8a46f572b6db06b
Sanghyuneee <pshyun1331@insurparts.com>
2023-02-05 15:45:01 +0900

    [ADD]Shop, Menu, Order, OrderFood Model Created for DevOps test

---
+++ order/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=64)
+    address = models.CharField(max_length=128)
+
+class Menu(models.Model):
+    shop = models.ForeignKey(Shop,on_delete=models.CASCADE)
+    food_name = models.CharField(max_length = 64)
+
+class Order(models.Model):
+    shop = models.ForeignKey(Shop,on_delete=models.CASCADE)
+    order_date = models.DateTimeField('date ordered')
+    address = models.CharField(max_length=128)
+    delivery_finish = models.BooleanField(default=0)
+    estimated_time = models.IntegerField(default=-1)
+class OrderFood(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    food_name = models.CharField(max_length=64)
+    
